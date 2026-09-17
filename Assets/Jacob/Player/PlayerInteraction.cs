5c3f939d08666ebba9ac4e8b49a2a7b9c4e7d378
Jacccobyiezx9 <167274314+Jacccobyiezx9@users.noreply.github.com>
2026-09-17 18:01:00 +0800

    Player pick up updated with inventory
    Will improve tomorrow

--- Assets/Jacob/Player/PlayerInteraction.cs
+++ Assets/Jacob/Player/PlayerInteraction.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
+using System.Collections;
 
 public class PlayerInteraction : MonoBehaviour
 {
@@ -10,6 +12,7 @@ public class PlayerInteraction : MonoBehaviour
     [Header("Pickup")]
     [SerializeField] private float pickupRange = 3f;
     [SerializeField] private LayerMask pickupLayer;
+    [SerializeField] private float pickupSpeed = 12f;
 
     [Header("Throw")]
     [SerializeField] private float throwForce = 5f;
@@ -17,10 +20,24 @@ public class PlayerInteraction : MonoBehaviour
     public GameObject heldObject;
     private Rigidbody heldRigidbody;
 
-    // Update is called once per frame
+    public List<GameObject> heldItems = new List<GameObject>();
+    private int activeItemIndex = -1;
+
+    private bool isPickingUp;
+
     void Update()
     {
-        
+        if (heldObject != null && isPickingUp)
+        {
+            PickupLerp();
+        }
+
+        float scroll = Mouse.current.scroll.y.ReadValue();
+
+        if (scroll != 0 && heldItems.Count > 1)
+        {
+            SwitchItem(scroll > 0 ? 1 : -1);
+        }
     }
 
     void TryPickup()
@@ -33,17 +50,65 @@ public class PlayerInteraction : MonoBehaviour
 
             if (rb != null && item != null)
             {
+                if (heldObject != null)
+                {
+                    heldObject.SetActive(false);
+                }
+
                 heldObject = rb.gameObject;
                 heldRigidbody = rb;
 
+                heldItems.Add(heldObject);
+                activeItemIndex = heldItems.Count - 1;
+
                 heldRigidbody.isKinematic = true;
-                heldObject.transform.SetParent(holdPoint);
-
-                heldObject.transform.localPosition = Vector3.zero;
-                heldObject.transform.localRotation = Quaternion.identity;
-
                 item.Held(true);
+                heldObject.transform.SetParent(holdPoint, true);
+
+                isPickingUp = true;
             }
+        }
+    }
+
+    void SwitchItem(int direction)
+    {
+        if (heldObject != null)
+        {
+            heldObject.SetActive(false);
+        }
+
+        activeItemIndex += direction;
+
+        if (activeItemIndex >= heldItems.Count)
+        {
+            activeItemIndex = 0;
+        }
+        else if (activeItemIndex < 0)
+        {
+            activeItemIndex = heldItems.Count - 1;
+        }
+
+        heldObject = heldItems[activeItemIndex];
+        heldRigidbody = heldObject.GetComponent<Rigidbody>();
+
+        heldObject.SetActive(true);
+        heldObject.transform.SetParent(holdPoint, true);
+
+        isPickingUp = true;
+    }
+
+    void PickupLerp()
+    {
+        Transform obj = heldObject.transform;
+        obj.localPosition = Vector3.Lerp(obj.localPosition,Vector3.zero, pickupSpeed * Time.deltaTime);
+        obj.localRotation = Quaternion.Lerp(obj.localRotation, Quaternion.identity,pickupSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(obj.localPosition, Vector3.zero) < 0.01f && Quaternion.Angle(obj.localRotation, Quaternion.identity) < 1f)
+        {
+            obj.localPosition = Vector3.zero;
+            obj.localRotation = Quaternion.identity;
+
+            isPickingUp = false;
         }
     }
 
@@ -51,20 +116,52 @@ public class PlayerInteraction : MonoBehaviour
     void ThrowObject()
     {
         Item item = heldObject.GetComponent<Item>();
-
         if (item != null)
         {
             item.Held(false);
         }
 
+        heldItems.RemoveAt(activeItemIndex);
         heldObject.transform.SetParent(null);
-
         heldRigidbody.isKinematic = false;
+        Collider thrownCollider = heldObject.GetComponent<Collider>();
 
-        heldRigidbody.AddForce(playerCamera.transform.forward * throwForce, ForceMode.Impulse);
+        if (thrownCollider != null)
+        {
+            thrownCollider.enabled = false;
+            StartCoroutine(ReEnableCollider(thrownCollider));
+        }
+
+        heldRigidbody.AddForce( playerCamera.transform.forward * throwForce, ForceMode.Impulse);
 
         heldObject = null;
         heldRigidbody = null;
+        isPickingUp = false;
+
+        if (heldItems.Count == 0)
+        {
+            activeItemIndex = -1;
+            return;
+        }
+
+        if (activeItemIndex >= heldItems.Count)
+        {
+            activeItemIndex = 0;
+        }
+
+        heldObject = heldItems[activeItemIndex];
+        heldRigidbody = heldObject.GetComponent<Rigidbody>();
+
+        heldObject.SetActive(true);
+        heldObject.transform.SetParent(holdPoint, true);
+
+        isPickingUp = true;
+    }
+
+    IEnumerator ReEnableCollider(Collider col)
+    {
+        yield return new WaitForSeconds(0.15f);
+        col.enabled = true;
     }
 
     public void OnInteract(InputValue value)
@@ -72,11 +169,7 @@ public class PlayerInteraction : MonoBehaviour
         if (!value.isPressed)
             return;
 
-        if (heldObject == null)
-        {
-            TryPickup();
-        }
-        
+        TryPickup();
     }
 
     public void OnThrow(InputValue value)
@@ -89,19 +182,20 @@ public class PlayerInteraction : MonoBehaviour
             ThrowObject();
         }
     }
+
     private void OnDrawGizmosSelected()
     {
         if (playerCamera == null)
             return;
 
         Gizmos.color = Color.yellow;
-        Gizmos.DrawRay(playerCamera.transform.position,playerCamera.transform.forward * pickupRange);
+        Gizmos.DrawRay(playerCamera.transform.position, playerCamera.transform.forward * pickupRange);
 
         if (holdPoint != null)
         {
             Gizmos.color = Color.green;
-            Gizmos.DrawWireSphere( holdPoint.position,0.15f);
-            Gizmos.DrawLine(playerCamera.transform.position,holdPoint.position);
+            Gizmos.DrawWireSphere(holdPoint.position, 0.15f);
+            Gizmos.DrawLine(playerCamera.transform.position, holdPoint.position);
         }
     }
 }
