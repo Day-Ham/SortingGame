5d6eccc72bedfa188b5de49c8408d934543f14f8
Mica Santiago <michaelasantiago25@gmail.com>
2026-09-23 18:20:12 +0800

    MnK keybindings done with key swapping

---
+++ Assets/Mica/Scripts/UIToggles.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIToggles : MonoBehaviour
+{
+    [Header("Options")]
+    [SerializeField] private List<Toggle> toggles = new List<Toggle>();
+    [SerializeField] private List<GameObject> panels = new List<GameObject>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < toggles.Count; i++)
+        {
+            int index = i;
+
+            toggles[i].onValueChanged.AddListener(state =>
+            {
+                if (state)
+                {
+                    ShowPanel(index);
+                }
+            });
+        }
+    }
+
+    private void ShowPanel(int index)
+    {
+        for (int i = 0; i < panels.Count; i++)
+        {
+            panels[i].SetActive(i == index);
+        }
+    }
+}
